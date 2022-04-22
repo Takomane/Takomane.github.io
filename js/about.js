19e8f2bd032ef360b5301dffb6a07ee0df20ef1c
Nick McFarlin <nick.f.mcfarlin@gmail.com>
2022-04-21 21:14:55 -0400

    one step at a time

---
+++ js/about.js
@@ -0,0 +1,19 @@
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".div5 img");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition < screenPosition) {
+    content.classList.add("div5animation");
+    content.classList.remove("hidden");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".div4 img");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if ((contentPosition = screenPosition)) {
+    content.classList.add("div4animation");
+    content.classList.remove("hidden2");
+  }
+});
