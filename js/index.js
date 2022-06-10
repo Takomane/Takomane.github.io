8f396e2a4f59511162c55972d086dd8b16e565fa
Nick McFarlin <nick.f.mcfarlin@gmail.com>
2022-06-10 17:31:47 -0400

    dark mode work

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 const checkbox = document.getElementById("checkbox");
 checkbox.addEventListener("change", function () {
   document.body.classList.toggle("dark");
-  document.nav.classList.toggle("dark");
 });
